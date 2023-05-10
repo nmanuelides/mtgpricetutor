9a13e585fe666c2e4e1f52c4a1ebfb8ed11ffba3
Niko Manuelides <>
2023-05-10 13:29:13 -0300

    Deploy to gh pages

--- src/components/search-box/src/SearchBox.tsx
+++ src/components/search-box/src/SearchBox.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { debounce } from "lodash";
-import { fetchCards } from "../../../services/cards";
-import { getCardPrices, Card } from "../../../services/starCityGamesCardPrices";
+import { getCardPrices, getAutoCompleteSuggestions, Card } from "../../../services/starCityGamesCardPrices";
 import Tilt from '../../../hoc/Tilt';
 import { tiltOptions } from "../../../hoc/tiltOptions";
 import { DOLAR_PIRULO } from "../../../dolar-pirulo";
@@ -13,31 +12,21 @@ import LoadingIndicator from "../../loading-indicator/src/LoadingIndicator";
 const SearchBox = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState<string[]>([]);
   const [selectedCards, setSelectedCards] = useState<Card[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const debouncedSearchHandler = useRef(
-    debounce((searchTerm: string) => {
-      fetchCards(searchTerm).then(data => {
-        setSearchResults(data.data);
-      }).catch(error => {
+    debounce(async (searchTerm: string) => {
+      try {
+        const data = await getAutoCompleteSuggestions(searchTerm);
+        setSearchResults(data || []);
+      } catch (error) {
         console.error(error);
-      });
-    }, 300)
+      }
+    }, 250)
   ).current;
 
-  const onCardSelected = (selectedCardIndex: number) => {
-    setSearchResults([]);
-    setIsLoading(true)
-    getCardPrices(searchResults[selectedCardIndex]).then(results => {
-      setSelectedCards(results);
-      setIsLoading(false);
-    }).catch(error => {
-      setIsLoading(false);
-      console.error(error);
-    })
-  }
 
   useEffect(() => {
     if(searchTerm !== ''){
@@ -57,25 +46,53 @@ const SearchBox = () => {
     debouncedSearchHandler(searchTerm);
   }
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    debouncedSearchHandler.cancel();
+  const onCardSelected = async (selectedCardIndex: number) => {
+    const cardName = searchResults[selectedCardIndex];
     setSearchResults([]);
+    setIsLoading(true)
+    try {
+      console.log('onCardSelected searching for: '+cardName)
+      const results = await getCardPrices(cardName);
+      setSelectedCards(results);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const inputValue = inputRef.current?.value;
+    let cardName = '';
+
+    inputValue && searchResults.forEach((cardSuggestion: string) => {
+      if (cardSuggestion.toLowerCase() === inputValue.toLowerCase()) {
+        cardName = cardSuggestion;
+      }
+    });
+
+    if (!cardName) {
+      return;
+    }
+    setSearchResults([]);
+    debouncedSearchHandler.cancel();
     setIsLoading(true);
-    inputValue && getCardPrices(inputValue).then(results => {
+    try {
+      console.log('onSubmit searching for: '+cardName)
+      const results = await getCardPrices(cardName);
       setSelectedCards(results);
+    } catch (error) {
+      console.error(error);
+    } finally {
       setIsLoading(false);
-    }).catch(error => {
-      setIsLoading(false);
-      console.error(error)
-    })
-  }
+    }
+  };
   return (
     <form className={selectedCards.length > 0 ? 'search-box__with-results' : 'search-box'} onSubmit={onSubmit}>
       <div className={'search-box__input-container'}>
         <div className='search-box__input-text-container'>
-        <input name='searchInput' className={isLoading ? 'search-box__input-text-disabled' : 'search-box__input-text'} type="text" value={searchTerm} ref={inputRef} onChange={onInputChange} autoComplete="off" placeholder="las mayúsculas y minúsculas importan..." disabled={isLoading}/>
+        <input name='searchInput' className={isLoading ? 'search-box__input-text-disabled' : 'search-box__input-text'} type="text" value={searchTerm} ref={inputRef} onChange={onInputChange} autoComplete="off" placeholder="Ingresá el nombre de una carta" disabled={isLoading}/>
         <LoadingIndicator isLoading={isLoading}/>
         </div>
         <button className={isLoading ? 'search-box__button-loading' :'search-box__button'} type='submit'disabled={isLoading}/>
@@ -83,8 +100,8 @@ const SearchBox = () => {
       <div className={'search-results-container'}>
       {searchResults.length > 0 &&
         <ul className={'search-results-container__list'}>
-          {searchResults.map((card, index) => {
-            return <li className={'search-results-container__result-item'} key={card} onClick={() => onCardSelected(index)}>{card}</li>
+          {searchResults.map((cardSuggestion, index) => {
+            return <li className={'search-results-container__result-item'} key={cardSuggestion} onClick={() => onCardSelected(index)}>{cardSuggestion}</li>
           })}
         </ul>
       }
